350379dbe10002b7649efcaa751a603535ca8544
김승현 <gimseunghyeon@gimseunghyeon-ui-MacBookAir.local>
2026-05-02 04:09:52 +0900

    최종 로그인 페이지 절대경로 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import GoogleLoginButton from '../../components/GoogleLoginButton';
+import GoogleLoginButton from '@/components/GoogleLoginButton';
 
 export default function LoginPage() {
   return (
